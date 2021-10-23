c487fdfae60193825fc6543e75675f94b1c61ab8
papi-boot <papi8261@gmail.com>
2021-10-24 05:37:29 +0800

    change

--- dist/postcss.config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    plugins: [require("postcss-preset-env"), require("autoprefixer")],
-};
--- dist/webpack.prod.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const webpack_merge_1 = require("webpack-merge");
-const webpack_common_1 = __importDefault(require("./webpack.common"));
-exports.default = webpack_merge_1.merge(webpack_common_1.default, Object.assign(Object.assign({}, webpack_common_1.default), { mode: "production", devtool: "inline-source-map" }));
